7ede77e0fb05fc24b11f37d68b58e7aca8ad09f0
Himanshu Sharma <sharmaslov@gmail.com>
2024-12-04 20:45:17 +0530

    minor

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -19,13 +19,13 @@ const Nav = () => {
       <div className="flex space-x-4">
         <div>
           {/* Contact Us Button */}
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Contact Us
           </button>
         </div>
         <div>
           {/* Services Button */}
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Services
           </button>
         </div>
